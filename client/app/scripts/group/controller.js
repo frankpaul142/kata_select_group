df0fd167b92fe3fe0027e7b7f696749c23e39398
Franklin Paula <frankpaul142@gmail.com>
2017-11-21 14:23:53 -0500

    finished

--- client/app/scripts/group/controller.js
+++ client/app/scripts/group/controller.js
@@ -2,34 +2,55 @@
 
 angular.module('Group')
 .controller('group', function ($scope) {
-  $scope.controller_loaded = 'Group loaded!';
-  $scope.getMembers=function(groups){
-	var groupedByCount = _.countBy(groups, function (item) {
-	    return item.id;
-	});
-	var grouped= _.groupBy(groups,'group');
-	var result=_.map(groupedByCount, function(value,key){ 
-			if(value>1)
+	$scope.controller_loaded = 'Group loaded!';
+	$scope.inputs=[];
+	$scope.result='';
+	var result=[];
+	$scope.getMembers=function(groups){
+		var groupedByCount = _.countBy(groups, function (item) {
+			return item.id;
+		});
+		var all_equals=!!_.reduce(groupedByCount,function(a, b){ return (a === b) ? a : NaN; });
+		var grouped= _.groupBy(groups,'group');
+		if(all_equals && groups.length !== Object.keys(groupedByCount).length){
+			return [grouped[1][0].id];
+		}
+		if(all_equals && groups.length === Object.keys(groupedByCount).length){
+			_.each(grouped, function(value){ 
+					result.push(value[0].id);			
+			});
+			result = _.sortBy(result, function(num) {
+				return num;
+			});
+			return result;
+		}
+		result=_.map(groupedByCount, function(value,key){ 
+		if(value>1){
 			return key;
-			
-  	});
-  	result=_.without(result,undefined);
-  	_.each(grouped, function(value,key){ 
-  			if(_.contains(result, value[0].id)||_.contains(result, value[1].id))
-  				return;
-  				result.push[value[0].id];
-	});
-  	return result;
-  }
-  $scope.asign=function(){
-  	for(var i=1;i<=$scope.quantity_teams;i++){
-  		$('#inputs').append('<div class="row"><div class="col-sm-4"><input ng-model="Group['+i+'][0]" /></div><div class="col-sm-4"><input ng-model="Group['+i+'][1]" /></div></div>');
-  	}
-  }
-  $scope.createArray=function(){
-  		console.log($scope.Group);
-	}
-
+		} 
+		});
+		result=_.without(result,undefined);
+		_.each(grouped, function(value){ 
+			if(_.contains(result, value[0].id) || _.contains(result, value[1].id)){
+				return;
+			}
+			result.push(value[0].id);				
+		});
+		result = _.sortBy(result, function(num) {
+		return num;
+		});
+		return result;
+	};
+	$scope.asign=function() {
+			$scope.inputs=[];
+		_($scope.quantity_teams).times(function(n){ 
+			$scope.inputs.push({'id':n+1,'group':n+1});
+			$scope.inputs.push({'id':n+1,'group':n+1}); 
+		});
+	};
+	$scope.calculate=function(){
+		$scope.result = $scope.getMembers($scope.inputs);
+	};
 })
 .config(function ($routeProvider) {
   $routeProvider
--- client/app/scripts/group/specs/controller_spec.js
+++ client/app/scripts/group/specs/controller_spec.js
@@ -17,60 +17,78 @@ describe('Controller: select group', function () {
       expect(scope.controller_loaded).toContain('loaded');
     });
   });
-
+  describe('asign function', function () {
+    it('scope.inputs length should be 10', function () {
+      scope.quantity_teams=5;
+      scope.asign();
+      expect(scope.inputs.length).toEqual(10);
+    });
+  });
   describe('Get min members', function () {
     it('should return an array with 2011', function () {
       var groups = [
-        {
-            id: '1009',
-            group: 1
-        }, {
-            id: '2011',
-            group: 1
-        }, {
-            id: '1017',
-            group: 2
-        }, {
-            id: '2011',
-            group: 2
-        }
+        {id: '1009', group: 1}, {id: '2011', group: 1},
+        {id: '1017', group: 2}, {id: '2011', group: 2}
       ];
 
       var result=scope.getMembers(groups);
       expect(result).toEqual(['2011']);
     });
+    it('should return an array with 1009', function () {
+      var groups = [
+        {id: '1009', group: 1}, {id: '2011', group: 1},
+        {id: '1009', group: 2}, {id: '2011', group: 2}
+      ];
+
+      var result=scope.getMembers(groups);
+      expect(result).toEqual(['1009']);
+    });
+    it('should return an array with 1003,1004', function () {
+      var groups = [
+        {id: '1003', group: 1}, {id: '2011', group: 1},
+        {id: '1004', group: 2}, {id: '2012', group: 2}
+      ];
+
+      var result=scope.getMembers(groups);
+      expect(result).toEqual(['1003','1004']);
+    });
     it('should return an array with 1009, 2002', function () {
       var groups = [
-        {
-            id: '1009',
-            group: 1
-        }, {
-            id: '2000',
-            group: 1
-        }, {
-            id: '1009',
-            group: 2
-        }, {
-            id: '2001',
-            group: 2
-        }, {
-            id: '1002',
-            group: 3
-        }, {
-            id: '2002',
-            group: 3
-        }, {
-            id: '1003',
-            group: 4
-        }, {
-            id: '2002',
-            group: 4
-        }
+        { id: '1009', group: 1}, { id: '2000', group: 1},
+        { id: '1009', group: 2}, { id: '2001', group: 2},
+        { id: '1002', group: 3}, { id: '2002', group: 3},
+        { id: '1003', group: 4}, { id: '2002', group: 4}
       ];
+
       var result=scope.getMembers(groups);
       expect(result).toEqual(['1009','2002']);
     });
+    it('should return an array with 1002, 2002, 2003', function () {
+      var groups = [
+        { id: '1000', group: 1}, { id: '2002', group: 1},
+        { id: '1002', group: 2}, { id: '2001', group: 2},
+        { id: '1003', group: 3}, { id: '2002', group: 3},
+        { id: '1004', group: 4}, { id: '2003', group: 4},
+        { id: '1005', group: 5}, { id: '2003', group: 5},
+        { id: '1006', group: 6}, { id: '2003', group: 6}
+      ];
+      
+      var result=scope.getMembers(groups);
+      expect(result).toEqual(['1002','2002','2003']);
+    });
   });
+
+  describe('Calculate', function () {
+    it('should asign the return of getMembers', function () {
+        spyOn(scope, 'getMembers').and.returnValue('array_with_results');
+        scope.inputs='inputs_object';
+        scope.calculate();
+
+        expect(scope.getMembers).toHaveBeenCalledWith('inputs_object');
+        expect(scope.result).toBe('array_with_results');
+
+      });
+   });
 
   describe('when going to /group', function () {
 
